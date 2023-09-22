3ea63eeeb3d677922cc4a94e4724e010c0d11b45
Sadgi Jakhar <sadgijakhar@Sadgis-MacBook-Air.local>
2023-09-22 15:17:14 +0530

    Binary To Decimal

--- lecture_3/BinaryToDecimal.java
+++ lecture_3/BinaryToDecimal.java
@@ -1,7 +1,20 @@
 package lecture_3;
 
+import java.util.Scanner;
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        int sum = 0;
+        while(n!= 0){
+            int c = n%10;
+            sum+= c*(int)Math.pow(2,i);
+            // System.out.println(c+" "+sum);
+            n = n/10;
+            i++;
+        }
+        System.out.println(sum);
+        sc.close();
     }
 }
