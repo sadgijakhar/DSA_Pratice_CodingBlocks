f195e6481d5b8b2ea07fb70b985bc17c13edd657
Sadgi Jakhar <sadgijakhar@Sadgis-MacBook-Air.local>
2023-11-15 06:34:15 +0530

    Updation in code

--- CodingBlocksAssignment/Leetcode/MinimumNumberOfOperationsToMakeArraysSimilar.java
+++ CodingBlocksAssignment/Leetcode/MinimumNumberOfOperationsToMakeArraysSimilar.java
@@ -6,12 +6,12 @@ public class MinimumNumberOfOperationsToMakeArraysSimilar {
     public static void main(String[] args) {
         int[] nums = {1, 2, 5};
         int[] target = {4, 1, 3};
-        ArrayList<Integer> evenArr1 = new ArrayList<>();
-        ArrayList<Integer> oddArr1 = new ArrayList<>();
-        ArrayList<Integer> evenArr2 = new ArrayList<>();
-        ArrayList<Integer> oddArr2 = new ArrayList<>();
+        ArrayList<Integer> evenArr1 = new ArrayList<>(nums.length);
+        ArrayList<Integer> oddArr1 = new ArrayList<>(nums.length);
+        ArrayList<Integer> evenArr2 = new ArrayList<>(nums.length);
+        ArrayList<Integer> oddArr2 = new ArrayList<>(nums.length);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
                 evenArr1.add(nums[i]);
             } else {
@@ -28,55 +28,27 @@ public class MinimumNumberOfOperationsToMakeArraysSimilar {
         Collections.sort(oddArr1);
         Collections.sort(evenArr2);
         Collections.sort(oddArr2);
-        int sum1 = 0;
-        for (int num : nums) {
-            sum1 += num;
-        }
-        int sum2 = 0 ;
-        for (int num : target) {
-            sum2 += num;
-        }
-        int count1 = 0;
-        int count2 = 0;
-        if(sum1 == sum2){
-            int n = evenArr1.size();
+        int diff =0;
+        int operation =0;
+        for(int i = 0 ;  i <  oddArr1.size() ;i++){
+            diff += oddArr1.get(i)-oddArr2.get(i);
+            if (oddArr1.get(i) > oddArr2.get(i)) {
+                operation += (oddArr1.get(i)-oddArr2.get(i))/2;
+            } 
 
-            for (int i = 0; i < n; i++) {
-                while (evenArr1.get(i) != evenArr2.get(i)) {
-                    if (evenArr1.get(i) < evenArr2.get(i)) {
-                        count2++;
-                        evenArr1.set(i, evenArr1.get(i) + 2);
-                    } else {
-                        count1++;
-                        evenArr1.set(i, evenArr1.get(i) - 2);
-                    }
-                }
-            }
-            int n1 = oddArr1.size();
-
-            for (int i = 0; i < n1; i++) {
-                while (oddArr1.get(i) != oddArr2.get(i)) {
-                    if (oddArr1.get(i) < oddArr2.get(i)) {
-                        count2++;
-                        oddArr1.set(i, oddArr1.get(i) + 2);
-                    } else {
-                        count1++;
-                        oddArr1.set(i, oddArr1.get(i) - 2);
-                    }
-                }
-            }
+        }
+        for(int i = 0 ;  i <  evenArr1.size() ;i++){
+            diff += evenArr1.get(i)-evenArr2.get(i);
+            if (evenArr1.get(i) > evenArr2.get(i)) {
+                operation += (evenArr1.get(i)-evenArr2.get(i))/2;
+            } 
+        }
+        if(diff == 0){
+           System.out.println(operation) ;
         }
         else{
-            System.out.println(-1);
-            return;
+           System.out.println(-1);
         }
         
-
-        if (count1 == count2) {
-            System.out.println(count1);
-        }
-        else{
-            System.out.println(0);
-        }
     }
 }
